94849a77175b65278f9bb4ac7eddc1470555ebb0
Oliver Kreylos <kreylos@cs.ucdavis.edu>
2012-02-21 16:41:22 -0800

    Release version 2.0
    Using tarball (with SHA1):
    91435ddd2f9390294c4034cf76029fb145e995cc *Kinect-2.0.tar.gz

--- CalibrateCameras.cpp
+++ CalibrateCameras.cpp
@@ -2,7 +2,7 @@
 CalibrateCameras - Simple utility to read calibration tie points between
 a depth camera and a color camera, and calculate the optimal projective
 transformation mapping color to depth.
-Copyright (c) 2010-2011 Oliver Kreylos
+Copyright (c) 2010 Oliver Kreylos
 
 This file is part of the Kinect 3D Video Capture Project (Kinect).
 
@@ -22,6 +22,8 @@ Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 02111-1307 USA
 ***********************************************************************/
 
+#include <stdlib.h>
+#include <string.h>
 #include <iostream>
 #include <IO/File.h>
 #include <IO/OpenFile.h>
@@ -29,15 +31,44 @@ Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 #include <Math/Math.h>
 #include <Math/Matrix.h>
 
-int main(void)
+int main(int argc,char* argv[])
 	{
+	/* Parse the command line: */
+	int imgSize[2]={640,480};
+	const char* tiePointFileName="CalibrationData.csv";
+	const char* matrixFileName="CameraCalibrationMatrices.dat";
+	int nameState=0;
+	for(int i=1;i<argc;++i)
+		{
+		if(argv[i][0]=='-')
+			{
+			if(strcasecmp(argv[i]+1,"size")==0)
+				{
+				for(int j=0;j<2;++j)
+					{
+					++i;
+					imgSize[j]=atoi(argv[i]);
+					}
+				}
+			}
+		else if(nameState==0)
+			{
+			tiePointFileName=argv[i];
+			++nameState;
+			}
+		else if(nameState==1)
+			{
+			matrixFileName=argv[i];
+			++nameState;
+			}
+		}
+	
 	/* Create the linear system: */
 	Math::Matrix a(12,12,0.0);
 	
 	{
 	/* Open the calibration data file: */
-	IO::AutoFile dataSource(IO::openFile("CalibrationData.csv"));
-	IO::CSVSource data(*dataSource);
+	IO::CSVSource data(IO::openFile(tiePointFileName));
 	
 	unsigned int numEntries=0;
 	while(!data.eof())
@@ -46,8 +77,8 @@ int main(void)
 		double x=data.readField<double>();
 		double y=data.readField<double>();
 		double z=data.readField<double>();
-		double s=data.readField<double>()/640.0;
-		double t=data.readField<double>()/480.0;
+		double s=data.readField<double>()/double(imgSize[0]);
+		double t=data.readField<double>()/double(imgSize[1]);
 		
 		/* Insert the entry's two linear equations into the linear system: */
 		double eq[2][12];
@@ -110,9 +141,8 @@ int main(void)
 			hom.set(i,j,qe.first(i*4+j,minEIndex)/scale);
 	
 	{
-	/* Open the calibration data file: */
-	IO::AutoFile dataSource(IO::openFile("CalibrationData.csv"));
-	IO::CSVSource data(*dataSource);
+	/* Open the calibration data file again: */
+	IO::CSVSource data(IO::openFile(tiePointFileName));
 	
 	/* Test the homography on all calibration data entries: */
 	while(!data.eof())
@@ -122,8 +152,10 @@ int main(void)
 		for(unsigned int i=0;i<3;++i)
 			world.set(i,data.readField<double>());
 		world.set(3,1.0);
-		double s=data.readField<double>();
-		double t=data.readField<double>();
+		
+		/* Skip s and t: */
+		data.readField<double>();
+		data.readField<double>();
 		
 		/* Apply the homography: */
 		Math::Matrix str=hom*world;
@@ -132,19 +164,24 @@ int main(void)
 	}
 	
 	/* Open the calibration file: */
-	IO::AutoFile matrixFile(IO::openFile("CameraCalibrationMatrices.dat",IO::File::WriteOnly));
-	matrixFile->setEndianness(IO::File::LittleEndian);
+	IO::FilePtr matrixFile(IO::openFile(matrixFileName,IO::File::WriteOnly));
+	matrixFile->setEndianness(Misc::LittleEndian);
 	
 	/* Create the depth projection matrix: */
 	Math::Matrix depthProjection(4,4,0.0);
-	double cameraFov=560.0;
-	depthProjection(0,0)=1.0/cameraFov;
-	depthProjection(0,3)=-320.0/cameraFov;
-	depthProjection(1,1)=1.0/cameraFov;
-	depthProjection(1,3)=-240.0/cameraFov;
+	double depthScale=34681.3;
+	double depthBias=1091.71;
+	double hScale=1.0/320.0/1.8530/1.0072;
+	double hCenter=320.0;
+	double vScale=1.0/320.0/1.8530/1.0072;
+	double vCenter=240.0;
+	depthProjection(0,0)=hScale;
+	depthProjection(0,3)=-hCenter*hScale;
+	depthProjection(1,1)=vScale;
+	depthProjection(1,3)=-vCenter*vScale;
 	depthProjection(2,3)=-1.0;
-	depthProjection(3,2)=-1.0/34400.0;
-	depthProjection(3,3)=1090.0/34400.0;
+	depthProjection(3,2)=-1.0/depthScale;
+	depthProjection(3,3)=depthBias/depthScale;
 	
 	/* Save the depth projection matrix: */
 	for(unsigned int i=0;i<4;++i)
